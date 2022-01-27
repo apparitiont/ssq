555999c064b20b0a19f4d503dc01f2df6ae70a38
dengt7 <412422050@qq.com>
2022-01-27 12:12:57 +0800

    添加排序

--- ssq.rb
+++ ssq.rb
@@ -2,7 +2,7 @@ def ssq(t)
 	red  = *(1..33).map {|r| r = "%02d" % r}
 	blue = *(1..16).map {|b| b = "%02d" % b}
 	t.to_i.times do
-		puts "红球: #{red.sample(6).join(" ")} 蓝球：#{blue.sample(1)[0].to_s}"
+		puts "红球: #{red.sample(6).sort.join(" ")} 蓝球：#{blue.sample(1)[0].to_s}"
 	end
 end
 ssq(ARGV[0])
